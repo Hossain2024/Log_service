1490ee60a9c75de393f297e167f68b30a5a3ffe3
Maliha <hossainmaliha18@gmail.com>
2026-09-20 22:36:43 -0700

    done

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.database import engine
 
 from app.database import Base, engine
 from app.routers.logs import router as logs_router
@@ -15,6 +16,13 @@ app = FastAPI(
 app.include_router(logs_router)
 
 
+
+@app.get("/database-info")
+def database_info():
+    return {
+        "database": engine.dialect.name,
+        "driver": engine.dialect.driver
+    }
 @app.get("/health", tags=["Health"])
 def health_check():
     return {"status": "healthy"}
